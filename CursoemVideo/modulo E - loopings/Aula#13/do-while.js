fbdc1f16ecee26f123f7965c5c143b5ce8335f7d
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-02-25 21:06:18 -0300

    loopings review

---
+++ CursoemVideo/modulo E - loopings/Aula#13/do-while.js
@@ -0,0 +1,19 @@
+/*                          While
+
+            var c = 1
+
+            while (c <= 10){
+                console.log(`Passo ${c}`)
+                c++                                                         // c = c + 1 
+            }
+
+*/
+
+//                          Do-While
+
+var c = 1
+
+do{
+    console.log(`Passo ${c}`)
+    c++                                                         
+}while (c <=15)
