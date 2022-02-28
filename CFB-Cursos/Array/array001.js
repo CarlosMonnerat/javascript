bfe6a38eb83f479237695ea331f95b37b48f6acd
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-02-28 16:45:33 -0300

    array.push( ) - estudos

---
+++ CFB-Cursos/Array/array001.js
@@ -0,0 +1,31 @@
+/*                          DECLARANDO UM ARRAY
+
+            var cor = new Array();      ou      var cor = [];
+
+                           
+                            ADICIONANDO ELEMENTOS AO ARRAY
+Ex. 01:
+var cor = [];
+
+cor.push("preto");                                      // add um elemento ao final do array
+cor.push("Branco");
+cor.push("Vermelho");
+cor.push("Verde");
+
+
+console.log(cor[0]);                                    //Informa que elemento está no índice informado
+console.log(cor[3]);
+
+
+Ex.02:
+*/
+var cor = []
+
+cor[0] = "preto";
+cor[1] = "Branco";
+cor[2] = "vermelho"
+cor[3] = "verde"
+cor.push("Rosa");
+
+console.log(cor[3]);
+console.log(cor[4]);
---
+++ CFB-Cursos/Array/array002.js
@@ -0,0 +1,14 @@
+var itens = ["poção", "chave", "cura", "capa", "espada", "anel"];
+var mochila = [];
+
+/*  mochila.push(itens[2]);             add ao 'vetor mochila' o elemento de índice 2 do 'vetor itens'
+    console.log(mochila[0]);
+*/
+
+mochila.push(4);
+mochila.push(0);
+mochila.push(3);
+
+console.log(itens[mochila[0]] );
+console.log(itens[mochila[1]] );
+console.log(itens[mochila[2]] );         
