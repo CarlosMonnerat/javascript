3c75dd1bb513f27eedd11d1d10ae35887d31fbb7
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-03-01 00:27:36 -0300

    introduction to matrices

---
+++ CFB-Cursos/Matrices/matriz001.js
@@ -0,0 +1,18 @@
+//                  Introdução a Matrizes
+
+var mochila = new Array();
+
+var item1 = ["corda",2],
+    item2 = ["faca",1],
+    item3 = ["cura",5],
+    item4 = ["prego",35];
+
+mochila.push(item1);
+mochila.push(item2);
+mochila.push(item3);
+mochila.push(item4);
+
+console.log(mochila[0][0] + "-" + mochila[0][1]);
+console.log(mochila[1][0] + "-" + mochila[1][1]);
+console.log(mochila[2][0] + "-" + mochila[2][1]);
+console.log(mochila[3][0] + "-" + mochila[3][1]);
