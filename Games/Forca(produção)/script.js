fd5331e61e8f8d335dab8a801339e7ddce1c38a0
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-03-14 15:57:29 -0300

    verifica e armazena as letras

--- Games/Forca(produção)/script.js
+++ Games/Forca(produção)/script.js
@@ -15,29 +15,20 @@ var acertos;
 var erros=0;
 var errosMax=7;
 //var desenhos=[];
-//var acertou=false;
+var acertou=false;
 var jogando=false;
 
 
 function iniciar(){
     jogando=true;
-    //cxJog.value="";
     cxJog.focus();                      //Mantém o cursor na caixa do jogador
     acertos=0;
     erros=0;
-    res.innerHTML="Letras Digitadas:"+"<br>"+cxJog.value;
-    //pos=Math.round(Math.random()*qtde);
-    //palavra=biblioteca[pos];
-    //tam=palavra.length;
     defLetras(tam);
-    //document.getElementById("").innerHTML="";
-    
-    alert("Palavra: "+palavra+"\n"+"Qtde de Letras: "+tam+"\n"+ "Letra digitada: "+cxJog.value)
     
     
+    //alert("Palavra: "+palavra+"\n"+"Qtde de Letras: "+tam+"\n"+ "Letra digitada: "+cxJog.value)
     
-    
-
 }
 
 function teste(){
@@ -54,4 +45,49 @@ function defLetras(d){
         obj=document.getElementById("letra"+i).style.display="inline-block";
     }
 
+}
+
+function jogar(){
+    if(cxJog.value==""){
+        alert("Digite uma Letra!")
+    }
+    
+    if(jogando=true){
+        var obj;
+        var letraTmp; //Vai receber a posição de cada letra
+        var pesq;
+        var letra=cxJog.value;
+        cxJog.value="";
+        cxJog.focus();
+        acertou=false;
+        pesq=palavra.match(letra); //Verifica se a letra digitada existe dentro da palavra
+        while(pesq!=null){ //Se for diferente de 'NUll' significa que a letra foi encontrada
+            letraTmp=palavra.search(letra); //Pesquisa por todas as ocorrencias dentro da palavra.
+            obj=document.getElementById("letra"+letraTmp).value=letra; //Armazena a letra digitada nas suas devidas posições 
+            palavra=palavra.replace(letra,'0'); //Substitui a letra da palavra por '0', para que não dê como ocorrência novamente
+            acertos++;
+            pesq=palavra.match(letra);
+            acertou=true;
+        }
+        if(!acertou){
+            //document.getElementById("textoinicial").innerHTML+="Letras Digitadas: <br>";
+            document.getElementById("letrasdig").innerHTML+=letra.toUpperCase()+",";
+            erros++;
+        
+        }
+
+        
+
+
+    }
+    
+
+
+}
+function NovaPalavra(){
+    res.innerHTML="Letras Digitadas:"+"<br>"+cxJog.value;
+    pos=Math.round(Math.random()*qtde);
+    palavra=biblioteca[pos];
+    tam=palavra.length;
+    defLetras(tam);
 }
