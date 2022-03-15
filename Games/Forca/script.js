2ba74f1455e2b311e3b0ed322ed95f91a340f5a0
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-03-15 17:15:51 -0300

    upgrade hangman game

--- Games/Forca/script.js
+++ Games/Forca/script.js
@@ -3,9 +3,13 @@ var btjogar=document.getElementsByClassName("btjogar")
 var btnova=document.getElementsByClassName("btnova")
 var res = document.getElementById("jaUsado")
 
-var biblioteca = ["brinquedo", "bicicleta", "carro", "televisao", "dinheiro", "shopping", "brincadeira","curso","computador","transporte","livraria",
-"tecnologia","churrasco","impressora","telefone","internet","portaria","escola","carnaval","teclado","guitarra","bateria","chinelo","helicoptero","aviaçao","faculdade",
-"martelo"];
+var biblioteca = ["brinquedo","bicicleta", "carro", "televisao", "dinheiro", "shopping", "brincadeira","curso","computador","transporte","livraria",
+"tecnologia","churrasco","impressora","telefone","internet","portaria","escola","carnaval","teclado","guitarra","bateria","chinelo","helicoptero","aviaçao",
+"faculdade","martelo","amarelo","amigo","amor","branco","boneca","celular","elefante","furacao","girafa","papagaio","janela","limonada","passarinho","peixe",
+"chaleira","onibus","caminhao","motocicleta","gelatina","parque","geladeira","cachorro","basquete","assombraçao","ardiloso","caminho","champanhe","chuveiro",
+"coelho","contexto","coraçao","esqueleto","esquerda","impacto","modernidade","quarentena","refeiçao","reportagem","campeonato","amendoim","pedreiro","moeda",
+"xicara","vermelho","serenata","biblioteca","caatinga","honra","cultura","exceçao","ambiçao","excesso","piedade","abstrato","paradoxo","comunhao","esperança",
+"anatomia","nostalgia","compaixao","excelente","dicionario","disciplina","felicidade","significado","importante","conveniente","melancia","morango","morcego"];
 var qtde=biblioteca.length-1;
 var pos=Math.round(Math.random()*qtde); //'Math.round' Arredonda o número e 'Math.random' Gera um número aleatório entre 0 e "*qtde"
 var palavra=biblioteca[pos];
