046f1b60262b2021e1c167bd586267c255f2a896
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-03-22 16:30:13 -0300

    upgrade pong game (left slash)

--- Games/Pong(produção)/pong.js
+++ Games/Pong(produção)/pong.js
@@ -30,7 +30,6 @@ var bolaW=20, bolaH=20;
 
 //Direção
 var bolaX, bolaY;
-var jogY=0;
 var cpuY=0;
 
 //Velocidde
@@ -41,4 +40,66 @@ var velJog;
 //Controle
 var pontos=0;
 var tecla;
+var jogo=false;
+
+//Funções
+
+function inicializa(){ //Inicia as variáveis e add os eventos
+    velBola=velCpu=velJog=8;
+    btIniciar=document.getElementById("btIniciar");
+    btIniciar=addEventListener("click",iniciaJogo);
+    jogador=document.getElementById("dvJogador");
+    cpu=document.getElementById("dvCPU");
+    bola=document.getElementById("dvBola");
+    txtPontos=document.getElementById("Pontos");
+    document.addEventListener("keydown",teclaDw);
+    document.addEventListener("keyup",teclaUp);
+}
+
+function iniciaJogo(){
+    if(!jogo){
+        cancelAnimationFrame(frames); //Se não cancelar ficará uma chamada em cima da outra
+        jogo=true;
+        dirJy=0;
+        PosbolaX=posBolaX0;
+        PosbolaY=posBolaY0;
+        PosjogY=posJogY0;
+        PoscpuY=posCpuY0;
+        Game();
+    }
+}
+
+function Game(){
+    if(jogo){
+        controlajog();
+    }
+    frames=requestAnimationFrame(Game);
+}
+
+function controlajog(){
+    if(jogo){
+        PosjogY+=velJog*dirJy;
+        jogador.style.top=PosjogY+"px";
+    }
+}
+
+function teclaDw(){
+    tecla=event.keyCode;
+    if(tecla==38){ //Cima
+        dirJy=-1;
+    }else if(tecla==40){// Baixo
+        dirJy=+1;
+    }
+}
+
+function teclaUp(){
+    tecla=event.keyCode;
+    if(tecla==38){ //Cima
+        dirJy=0;
+    }else if(tecla==40){// Baixo
+        dirJy=0;
+    }
+}
+
+
 
