cb1626a69929c6e9eaa08037b52a6d99bc861942
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-03-22 15:12:42 -0300

    pong game create

---
+++ Games/Pong(produção)/pong.js
@@ -0,0 +1,44 @@
+//Elementos
+var btIniciar;
+var bola;
+var cpu;
+var jogador;
+var txtPontos;
+
+//Controle de Animação
+var game;
+var frames;
+
+//Posições
+var PosbolaX, PosbolaY;
+var PosjogX, PosjogY;
+var PoscpuX, PoscpuY;
+
+//Direção de acordo com a tecla
+var dirJy;
+
+//Posições iniciais
+var posBolaX0=475, posBolaY0=240;
+var posJogY0=180;
+var posCpuY0=180;
+
+//Tamanhos
+var campoX=0, campoY=0;
+var campoW=960, campoH=500;
+var barraW=20, barraH=140;
+var bolaW=20, bolaH=20;
+
+//Direção
+var bolaX, bolaY;
+var jogY=0;
+var cpuY=0;
+
+//Velocidde
+var velBola;
+var velCpu;
+var velJog;
+
+//Controle
+var pontos=0;
+var tecla;
+
