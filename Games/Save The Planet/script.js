33898c26da15f8a76f1a78ed363cf27781467d59
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-04-13 17:12:48 -0300

    STP bombs create

--- Games/Save The Planet/script.js
+++ Games/Save The Planet/script.js
@@ -1,10 +1,11 @@
 //Variáveis
-var Jog, velJ, velT;
+var Jog, velJ, velT, velB;
 var PosXJ, PosYJ, dirXJ, dirYJ;
 var jogo;
 var frames;
 var telaWidth, telaHeigth;
-
+var contB, painelContB, tempoB;
+var vidaPlaneta;
 
 
 //CONFIGURAÇÕES INICIAIS DO jOGO
@@ -22,7 +23,17 @@ function inicia(){
         velT=10;
         Jog.style.left=PosXJ+"px";
         Jog.style.top=PosYJ+"px";
+    //Controle das Bombas
+        clearInterval(tempoB); //Zera o intervalo de criação das bombas
+        contBombas=150;
+        velB=3;
+        tempoB=setInterval(addBomba,1700); //Cria um intervalo entre a criação de cada bomba
+    //Controles do Planeta
+        vidaPlaneta=100;
+
+    //Chama Funções
     gameLoop();
+    
 }
 
 //COMANDOS DAS TECLAS PRESSIONADAS
@@ -64,6 +75,7 @@ function gameLoop(){
         //Funções de controle
         controlaJog();
         controleTiros();
+        controlaBombas()
     }
     frames=requestAnimationFrame(gameLoop);
 }
@@ -95,8 +107,39 @@ function controleTiros(){
             if(posTiro<0){
                 tiros[i].remove();
             }
+        }
+    }
+}
+
+//IMPLEMENTANDO AS BOMBAS
+function addBomba(){
+    if(jogo){
+        var y=0;
+        var x=Math.random()*telaWidth; //Sorteia um número entre zero e a largura da tela
+        var bomba=document.createElement("div");
+        var att1=document.createAttribute("class");
+        var att2=document.createAttribute("style");
+        att1.value="bomba";
+        att2.value="top:"+y+"px; left:"+x+"px";
+        bomba.setAttributeNode(att1);
+        bomba.setAttributeNode(att2);
+        document.body.appendChild(bomba);
+        contB--;
+    }
+}
+function controlaBombas(){
+    var bombas=document.getElementsByClassName("bomba"); //Armazena todas as bombas criadas com a classe 'bomba'
+    var tam=bombas.length;
+    for(var i=0; i<tam; i++){
+        if(bombas[i]){
+            var posBomba=bombas[i].offsetTop; //Pega a posição Y de cada bomba
+            posBomba+=velB;  //Faz com que a bomba caia em direção ao final da tela.
+            bombas[i].style.top=posBomba+"px";
+            if(posBomba>telaHeigth){
+                vidaPlaneta-=10;
+                bombas[i].remove();
+            }
 
         }
-
     }
 }
