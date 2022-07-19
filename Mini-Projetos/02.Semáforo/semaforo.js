402d9f1b1cb5e2c6eb4ad422e494b5aeac8bfce1
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-07-19 17:13:24 -0300

    Update semaforo.js

--- Mini-Projetos/02.Semáforo/semaforo.js
+++ Mini-Projetos/02.Semáforo/semaforo.js
@@ -1,24 +1,42 @@
 const img = document.getElementById('img')
 const buttons = document.getElementById('buttons')
+let colorIndex = 0;
+let setIntervalId = null;           //Variável para parar o setInteval
 
 buttons.addEventListener('click',trafficLight)
 
-const turnOn = {                //Objeto que contém três funções
+const turnOn = {                    //Objeto que contém três funções
     'red':    () => img.src='imagens/vermelho.png',
     'yellow': () => img.src='imagens/amarelo.png',
     'green':  () => img.src='imagens/verde.png',
-    'automatic': () => setInterval(changeColor, 1000) //Executa a função a cada 1s
+    'automatic': () => setIntervalId= setInterval(changeColor, 1000)    //Executa a função a cada 1s
     // "setTimeout(changeColor,1000)" - Executa apenas uma vez dps de 1s
-
 }
 
 function trafficLight(event){
-    turnOn[event.target.id]();  //Chama um método de um Objeto
-
+    stopAutomatic();                //Para o setinterval anterior toda vez que é acionado
+    turnOn[event.target.id]();      //Chama um método de um Objeto
 }
 
 const changeColor = ()=>{
-    const color= 'red'
+    const colors= ['red','yellow','green']
+    const color= colors[colorIndex]
     turnOn[color]();
+    nextIndex();
+}
 
+const nextIndex = ()=>{
+   colorIndex = colorIndex < 2 ? ++colorIndex : 0;
+   
+    /* OU...
+        if(colorIndex < 2){
+            colorIndex++
+            }else{
+                colorIndex=0;
+        } 
+    */
+}
+
+const stopAutomatic= ()=>{
+    clearInterval(setIntervalId);
 }
