c8f8ec404a5f1d2ee2a4ee0f6043218c1f6a453a
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-07-25 17:54:21 -0300

    create function that plays the sounds

--- Mini-Projetos/04.Drumkit/drumkit.js
+++ Mini-Projetos/04.Drumkit/drumkit.js
@@ -25,10 +25,26 @@ const criarDiv = (texto) =>{
 
 const exibir = (sons) => Object.keys(sons).forEach(criarDiv)    //Para cada 'chave' do 'Json sons', chama a função 'criarDiv'
 /*
+    Ao usar 'const exibir = (sons) =>' pegamos todas as "chaves" e todos os "objetos". Para pegarmos apenas as "chaves" usamos "Object.keys()"
     'Object.keys()' - Pega todas as chaves(letras) do 'Obj Json' e retorna um array com todas elas
     'forEach()' - Percorre todos os elementos do Array
     - Como a Função não tem um retorno e só tem uma linha, não é necessário usar '{}'
 */
 
+const tocarSom = (letra) => {
+    const audio = new Audio(`sounds/${sons[letra]}`)
+    audio.play();
+
+}
+const ativarDiv = (evento) => {
+    const letra = evento.target.id
+    const letraPermitida = sons.hasOwnProperty(letra)       //Verifica se "letra" existe no Json "sons"
+    if(letraPermitida){
+        tocarSom(letra);
+    }
+}
+
+
 
 exibir(sons);
+document.getElementById('icontainer').addEventListener('click', ativarDiv);
