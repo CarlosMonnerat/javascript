8322c8f8be4e44abf440ae19af8565b93b6191df
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-11-10 15:37:53 -0300

    Update app.js

--- Mini-Projetos/07.Todo-List/app.js
+++ Mini-Projetos/07.Todo-List/app.js
@@ -1,9 +1,12 @@
 'use strict';
 
-/*
-    <label class="todo__item">
-        <input type="checkbox">
-        <div> teste de item 1 </div>
-        <input type="buttom" value="x">
-    </label>
-*/
+const criarItem = (tarefa, status) =>{
+    const item = document.createElement('label');
+    item.classList.add('todo__item');
+    item.innerHTML = `
+        <input type="checkbox" ${status}>
+        <div> ${tarefa} </div>
+        <input type="button" value="x">
+    `
+    document.getElementById('todoList').appendChild(item);
+}
