caa613b6379a3a1a729aca8d09ee93c4bac11669
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-11-10 16:03:07 -0300

    Update app.js

--- Mini-Projetos/07.Todo-List/app.js
+++ Mini-Projetos/07.Todo-List/app.js
@@ -1,6 +1,14 @@
 'use strict';
 
-const criarItem = (tarefa, status) =>{
+let banco = [                                                /*Este Array funciona como um banco de dados que armazena as tarefas digitadas pelo usuário */
+    {'tarefa':'Estudar JS', 'status': ''},
+    {'tarefa':'Ler um Livro', 'status': 'checked'},
+    {'tarefa':'Malhar', 'status': 'checked'},
+    {'tarefa':'Trabalhar', 'status': ''}
+
+]
+
+const criarItem = (tarefa, status) => {                      /* Cria as tarefas dentro do DOM*/
     const item = document.createElement('label');
     item.classList.add('todo__item');
     item.innerHTML = `
@@ -10,3 +18,11 @@ const criarItem = (tarefa, status) =>{
     `
     document.getElementById('todoList').appendChild(item);
 }
+
+const render = () => {                                       /* Lê o banco de dados e Atualiza a tela */
+    banco.forEach(item => criarItem(item.tarefa, item.status)); /*Para cada item do banco, pega um item e joga para a função 'criarItem()' informando seus parametros*/
+    
+
+}
+
+render();
