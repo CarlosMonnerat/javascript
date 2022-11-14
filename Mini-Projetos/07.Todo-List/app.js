49657546a42e6e98f2505dd12e647e3d87fa5f32
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-11-14 15:33:12 -0300

    Update app.js

--- Mini-Projetos/07.Todo-List/app.js
+++ Mini-Projetos/07.Todo-List/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let banco = [                                                /*Este Array funciona como um banco de dados que armazena as tarefas digitadas pelo usuário */
+let banco = [                                                        /*Este Array funciona como um banco de dados que armazena as tarefas digitadas pelo usuário */
     {'tarefa':'Estudar JS', 'status': ''},
     {'tarefa':'Ler um Livro', 'status': 'checked'},
     {'tarefa':'Malhar', 'status': 'checked'},
@@ -8,7 +8,7 @@ let banco = [                                                /*Este Array funcio
 
 ]
 
-const criarItem = (tarefa, status) => {                      /* Cria as tarefas dentro do DOM*/
+const criarItem = (tarefa, status) => {                              /* Cria as tarefas dentro do DOM*/
     const item = document.createElement('label');
     item.classList.add('todo__item');
     item.innerHTML = `
@@ -27,9 +27,9 @@ const clear = () => {                                               /*Limpa as t
 
 }
 
-const render = () => {                                          /* Lê o banco de dados e Atualiza a tela */
-    clear();                                                    /*Limpa a tela cada vez que o Render é chamado para não haver duplicações */
-    banco.forEach(item => criarItem(item.tarefa, item.status)); /*Para cada item do banco, pega um item e joga para a função 'criarItem()' informando seus parametros*/
+const render = () => {                                              /* Lê o banco de dados e Atualiza a tela */
+    clear();                                                        /*Limpa a tela cada vez que o Render é chamado para não haver duplicações */
+    banco.forEach(item => criarItem(item.tarefa, item.status));     /*Para cada item do banco, pega um item e joga para a função 'criarItem()' informando seus parametros*/
 
 
 }
