69ad35107494430812556f645324509524bcb27a
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-11-27 16:00:27 -0300

    update ex02

---
+++ CFB-Cursos/Object/exercises/ex02/calc.js
@@ -0,0 +1,7 @@
+const teclasNum = [...document.querySelectorAll(".num")]
+const teclasOp = [...document.querySelectorAll(".op")]
+const teclaRes = document.querySelector(".res")
+
+console.log(teclasNum)
+console.log(teclasOp)
+console.log(teclaRes)
