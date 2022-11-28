fa911b48df98a8df0561a5cf34496b46415beee2
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-11-28 15:53:56 -0300

    update ex02

--- CFB-Cursos/Object/exercises/ex02/calc.js
+++ CFB-Cursos/Object/exercises/ex02/calc.js
@@ -1,7 +1,23 @@
 const teclasNum = [...document.querySelectorAll(".num")]
 const teclasOp = [...document.querySelectorAll(".op")]
 const teclaRes = document.querySelector(".res")
+const display = document.querySelector(".display")
+const teclaOn = document.getElementById("tecla_on")
+const teclaLimpar = document.getElementById("teclaLimpar")
 
-console.log(teclasNum)
-console.log(teclasOp)
-console.log(teclaRes)
+teclasNum.forEach((el) =>{
+    el.addEventListener("click", (event) =>{
+        display.innerHTML += event.target.innerHTML
+    })
+})
+
+teclasOp.forEach((el) =>{
+    el.addEventListener("click", (event) =>{
+        display.innerHTML += event.target.innerHTML
+    })
+})
+
+teclaLimpar.addEventListener("click", (event) =>{
+    display.innerHTML = "0"
+})
+
