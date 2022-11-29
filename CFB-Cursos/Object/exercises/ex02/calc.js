1a1ef4ff964e46b71d4d55dcc6ab00910cbc2dde
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-11-28 21:20:46 -0300

    update ex02

--- CFB-Cursos/Object/exercises/ex02/calc.js
+++ CFB-Cursos/Object/exercises/ex02/calc.js
@@ -5,19 +5,57 @@ const display = document.querySelector(".display")
 const teclaOn = document.getElementById("tecla_on")
 const teclaLimpar = document.getElementById("teclaLimpar")
 
+let sinal = false
+let decimal = false
+
 teclasNum.forEach((el) =>{
     el.addEventListener("click", (event) =>{
-        display.innerHTML += event.target.innerHTML
+        if(sinal == true && event.target.innerHTML ==","){
+            display.innerHTML += "0,"
+            sinal = false
+            decimal = true
+        }else{
+            sinal = false
+            if(event.target.innerHTML == ","){
+                 if(!decimal){
+                    decimal = true
+                    display.innerHTML += event.target.innerHTML        
+                }   
+            }else{
+                if(display.innerHTML == "0"){
+                    display.innerHTML = ""
+                }
+                display.innerHTML += event.target.innerHTML
+            }
+        }
+        
+        
     })
 })
 
 teclasOp.forEach((el) =>{
     el.addEventListener("click", (event) =>{
-        display.innerHTML += event.target.innerHTML
+        decimal = false
+        if(!sinal){
+            sinal = true
+            if(display.innerHTML == "0"){
+                display.innerHTML = ""
+            }
+            if(event.target.innerHTML == "x"){
+                display.innerHTML += "*"
+            }else{
+                display.innerHTML += event.target.innerHTML
+            }
+            
+        }
+        
     })
 })
 
 teclaLimpar.addEventListener("click", (event) =>{
+    sinal = false
+    decimal = false
     display.innerHTML = "0"
+    
 })
 
