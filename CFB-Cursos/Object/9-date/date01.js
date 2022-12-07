fba1e32aa116f58f52f05f4ecf3065149f2d23b9
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-12-06 21:34:29 -0300

    Update date01.js

--- CFB-Cursos/Object/9-date/date01.js
+++ CFB-Cursos/Object/9-date/date01.js
@@ -1,14 +1,16 @@
 const data = new Date()
 
-let dia = data.getDate()
+let sem = data.getDay()                 //Pega o número dos dias da Semana. 0 = Domingo ... 6 = Sábado
+
+let dia = data.getDate()                //Pega os dias do Mês
 dia = dia < 10 ? "0"+dia : dia
 
-let mes = data.getMonth()
-mes = mes + 1                           //Os meses começam do zero, ou seja 0 = Janeiro .... 11 = Dezembro
+let mes = data.getMonth()               //Pega o número do Mês
+mes = mes + 1                           //Os meses começam do zero, ou seja 0 = Janeiro ... 11 = Dezembro
 mes = mes < 10 ? "0"+mes : mes
 
 
-let ano = data.getFullYear()
+let ano = data.getFullYear()            //Pega o ano com 4 digitos
 
 
-console.log(`${dia}/${mes}/${ano}`)
+console.log(`Dada: ${sem} ${dia}/${mes}/${ano}`)
