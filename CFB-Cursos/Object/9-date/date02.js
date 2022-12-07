f2efac116304ba3bea646d88e29393f11551a898
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-12-06 22:10:32 -0300

    Create date02.js

---
+++ CFB-Cursos/Object/9-date/date02.js
@@ -0,0 +1,18 @@
+// Criando um relógio
+
+const relogio = ()=>{
+    const data = new Date()
+    let hora = data.getHours()
+    hora = hora < 10 ? "0"+hora : hora
+
+    let minuto = data.getMinutes()
+    minuto = minuto < 10 ? "0"+minuto : minuto
+
+    let segundo = data.getSeconds()
+    segundo = segundo < 10 ? "0"+segundo : segundo
+    
+    console.log(`${hora}:${minuto}:${segundo}`)
+}
+ const intervalo = setInterval(relogio, 1000) //Chama a funcão relogio a cada 1000 milisegundos = 1 segundo
+
+
