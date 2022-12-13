a042a714ff0130866e78edc3520251c6830d918b
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-12-13 15:17:19 -0300

    Update date03.js

--- CFB-Cursos/Object/9-date/date03.js
+++ CFB-Cursos/Object/9-date/date03.js
@@ -7,10 +7,10 @@ const timer = document.getElementById("timer")
 
 //Variaveis
 const som_alarme = new Audio("alert.mp3")
-som_alarme.loop = -1 //O número indica a qtd de vezes que a ação será executada. Um nº negativo indica que a ação será executada infinitamente
+som_alarme.loop = -1                                //O número indica a qtd de vezes que a ação será executada. Um nº negativo indica que a ação será executada infinitamente
 
-let ts_atual = null //timestamp atual
-let ts_alarme = null // timestamp alarme
+let ts_atual = null                                 //timestamp atual
+let ts_alarme = null                                // timestamp alarme
 let alarme_ativado = false
 let alarme_tocando = false
 
@@ -31,4 +31,26 @@ btn_ativar.addEventListener("click",()=>{
     hora_alarme.innerHTML="Hora do Alarme:"+hora+":"+minuto+":"+segundo
 })
 
-btn_parar.addEventListener("click",()=>{})
+btn_parar.addEventListener("click",()=>{
+    alarme_ativado = false
+    alarme_tocando = false
+    hora_alarme.innerHTML="Hora do Alarme:"
+    tmp_alarme.value = 0
+    timer.classList.remove("alarme")
+    som_alarme.pause()
+    som_alarme.currentTime = 0                     //rebobina o tempo do audio para o inicio
+})
+
+//Função
+const relogio = ()=>{
+    const data = new Date()
+    if(alarme_ativado && !alarme_tocando){
+        if(data.getTime() >= ts_alarme){           //Se o Timestamp atual for maior ou igual ao tbm do alarme, o som deve ser emitido
+            alarme_tocando = true
+            som_alarme.play()
+            timer.classList.add("alarme")
+        }
+    }
+}
+
+const intervalo = setInterval(relogio,1000)
