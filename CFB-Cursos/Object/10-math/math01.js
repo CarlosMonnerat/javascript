c3cad78f4d204857a2e7b6d476c5be209f154cda
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-12-15 10:41:10 -0300

    math library  js

--- CFB-Cursos/Object/10-math/math01.js
+++ CFB-Cursos/Object/10-math/math01.js
@@ -1,6 +1,16 @@
 // Math é uma biblioteca matemática do JS
     // (X) 'const math = new Math()'  Math é uma biblioteca de classe estática, e por isso não precisa ser instanciada!
 
-const mat = Math.PI
+const mat = document.getElementById("mat")
 
-console.log(mat)
+//mat.innerHTML=Math.random()                       //Gera um número aleatório entre 0 e 1
+//mat.innerHTML=Math.random()*10                    //Gera um número aleatório entre 0 e 10
+//mat.innerHTML=Math.random()*100                   //Gera um número aleatório entre 0 e 100
+//mat.innerHTML=Math.floor(Math.random()*10)        //Gera apenas a parte inteira do número aleatório (entre 0 e 9)
+
+//const num=Math.random()*10
+//mat.innerHTML=Math.round(num)                     //Gera apenas a parte inteira do número aleatório (entre 0 e 10)
+
+//mat.innerHTML=Math.sqrt(64)                       //Retorna a raíz quadrada do número passado no parâmetro
+mat.innerHTML=Math.pow(10,2)                        //Retorna 10 elevado a 2
+
