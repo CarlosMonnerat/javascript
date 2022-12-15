744ea55bf801442601ae6e8fb3afa7337928cfcf
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-12-15 09:42:15 -0300

    Create math01.js

---
+++ CFB-Cursos/Object/10-math/math01.js
@@ -0,0 +1,6 @@
+// Math é uma biblioteca matemática do JS
+    // (X) 'const math = new Math()'  Math é uma biblioteca de classe estática, e por isso não precisa ser instanciada!
+
+const mat = Math.PI
+
+console.log(mat)
