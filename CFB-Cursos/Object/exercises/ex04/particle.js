688f7fc4cf10bb3c12e1e3816a962dbe72506316
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-12-23 16:29:48 -0300

    Update particle.js

--- CFB-Cursos/Object/exercises/ex04/particle.js
+++ CFB-Cursos/Object/exercises/ex04/particle.js
@@ -10,13 +10,43 @@ let bolas = []
 let numBolas = 0
 
 class Bola{
-    constructor(){
-        this.tam = Math.floor(Math.random()*15)+10                          //Tamanho
-        this.r = Math.floor(Math.random()*255)                              //Cor
+    constructor(arrayBolas, palco){
+    //Tamanho
+        this.tam = Math.floor(Math.random()*15)+10
+    //Cor                       
+        this.r = Math.floor(Math.random()*255)                              
         this.g = Math.floor(Math.random()*255)
         this.b = Math.floor(Math.random()*255)
-        this.px = Math.floor(Math.random()*(larguraPalco - this.tam))       //Posição
+    //Posição
+        this.px = Math.floor(Math.random()*(larguraPalco - this.tam))       
         this.py = Math.floor(Math.random()*(alturaPalco - this.tam))
+    //Velocidade
+        this.velx = Math.floor(Math.random()*2)+0.5
+        this.vely = Math.floor(Math.random()*2)+0.5
+    //Direção
+        this.dirx = (Math.random()*10) > 5 ? 1 : -1
+        this.diry = (Math.random()*2) > 5 ? 1 : -1
+    //Id de cada bolinha
+        this.palco = palco
+        this.arrayBolas = bolas
+        this.id = Date.now()+"_"+Math.floor(Math.random()*100000000000000)
+    //Controle
+        this.desenhar()
+        this.controle = setInterval(this.controlar,10)
+    //Relaciona bolinha instanciada no DOM com o ArrayBolas
+        this.eu = document.getElementById(this.id)
+    }
+    minhaPos=()=>{
+
+    }
+    remover=()=>{
+
+    }
+    desenhar=()=>{
+
+    }
+    controlar=()=>{
+
     }
 }
 
