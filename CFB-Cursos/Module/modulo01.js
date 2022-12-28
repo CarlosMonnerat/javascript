00b65d4c0ffca48b36e4a645afed03dc073bb003
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2022-12-28 15:37:31 -0300

    module

---
+++ CFB-Cursos/Module/modulo01.js
@@ -0,0 +1,8 @@
+/*
+    módulos em JavaScript são uma forma de compartilhar informações entre arquivos, 
+    dessa forma, todos os arquivos que realizarem a importação desse módulo poderão utilizar 
+    e usufruir de suas funções ou informações que foram expostas. Dessa maneira, evitamos ter 
+    que ficar carregando N arquivos .
+*/
+
+const cursos = {"javascript"}
