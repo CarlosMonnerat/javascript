64e29660b149fb3b42e4a9a3d29a851be8f8bd8e
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-01-06 14:49:34 -0300

    Update symbol.02.js

--- CFB-Cursos/Function/symbol.02.js
+++ CFB-Cursos/Function/symbol.02.js
@@ -7,12 +7,17 @@ class Jogador{
     }
 }
 
-let jogadores = [new Jogador("j1"),new Jogador("j2"),new Jogador("j3"),new Jogador("j4")]
+let jogadores = [new Jogador("j1"),new Jogador("j2"),new Jogador("j3"),new Jogador("j1"),new Jogador("j2"),new Jogador("j3")]
 
-//Removendo um elemento do Array pelo symbol
+//Removendo um elemento do Array pelo symbol (Elementos com o msm nome, não serão afetados)
 let s1=jogadores[0].id
 jogadores=jogadores.filter((j)=>{
-    return j.id!=s1
+    return j.id!=s1                         //Irá remover o elemento da posição [0]
+})
+
+//Removendo elemento do Array pelo nome (Todos os elementos com o msm nome, serão afetados)
+jogadores=jogadores.filter((j)=>{
+    return j.nome!="j2"                     //Irá retornar somente os elementos com o nome diferente de "j2"
 })
 
 console.log(jogadores)
