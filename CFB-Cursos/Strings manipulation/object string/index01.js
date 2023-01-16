f058f6dc96e64eea1d1ea08f3f80af418cee4ca3
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-01-16 19:35:38 -0300

    string manipulation

---
+++ CFB-Cursos/Strings manipulation/object string/index01.js
@@ -0,0 +1,8 @@
+let nome = new String("Bruno Pinho Campos")
+let canal = new String("CFBCursos")
+
+console.log(nome.charAt(0)) //Retorna o caractere da posição[0]
+console.log(nome.charCodeAt(0)) //Retorna o código (do teclado) relacionado ao caractere da posição[0]
+console.log(nome.concat(canal)) //Concatena a string 'nome' com a string 'canal'
+console.log(nome.indexOf("o")) //Retorna a posição do Primeiro caractere "o"
+console.log(nome.lastIndexOf("o")) //Retorna a posição do Último caractere "o"
