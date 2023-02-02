e77abe1f75fdcc2cb691151d28569ea0ddcbc510
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-02-02 18:52:40 -0300

    create animation02

---
+++ CFB-Cursos/Animation/aula02/aula02.js
@@ -0,0 +1,23 @@
+const carro = document.getElementById("carro")
+const btn_esquerda = document.getElementById("btn_esquerda")
+const btn_direita = document.getElementById("btn_direita")
+
+const init = ()=>{
+    carro.style.position="relative"
+    carro.style.left="0px"
+}
+
+btn_esquerda.addEventListener("click", ()=>{
+    let pos = parseInt(carro.style.left)
+    pos-=10
+    carro.style.left=`${pos}px`
+})
+
+btn_direita.addEventListener("click", ()=>{
+    let pos = parseInt(carro.style.left)
+    pos+=10
+    carro.style.left=`${pos}px`
+})
+
+window.onload=init
+//window.addEventListener("load",init())
