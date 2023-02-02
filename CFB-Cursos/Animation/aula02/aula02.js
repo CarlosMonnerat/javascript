4677e2d7577ecafe0fba2a29c84ae7d274aad31c
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-02-02 19:13:28 -0300

    animation setInterval

--- CFB-Cursos/Animation/aula02/aula02.js
+++ CFB-Cursos/Animation/aula02/aula02.js
@@ -1,23 +1,33 @@
 const carro = document.getElementById("carro")
 const btn_esquerda = document.getElementById("btn_esquerda")
 const btn_direita = document.getElementById("btn_direita")
+const btn_parar = document.getElementById("btn_parar")
 
 const init = ()=>{
     carro.style.position="relative"
     carro.style.left="0px"
 }
 
+let anima = null
+
+const mover = (dir)=>{
+    carro.style.left=parseInt(carro.style.left) + (10*dir) +"px"
+}
+
 btn_esquerda.addEventListener("click", ()=>{
-    let pos = parseInt(carro.style.left)
-    pos-=10
-    carro.style.left=`${pos}px`
+    clearInterval(anima)
+    anima = setInterval(mover,20,-1)            //Função, intervalo(milisegundos), Parametro
 })
 
 btn_direita.addEventListener("click", ()=>{
-    let pos = parseInt(carro.style.left)
-    pos+=10
-    carro.style.left=`${pos}px`
+    clearInterval(anima)
+    anima = setInterval(mover,20, 1)            //Função, intervalo(milisegundos), Parametro
 })
+
+btn_parar.addEventListener("click", ()=>{
+    clearInterval(anima)
+})
+
 
 window.onload=init
 //window.addEventListener("load",init())
