9c62cf48cdff0b01e9159593c62fd289083b3121
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-02-14 19:29:00 -0300

    Update validation.js

--- CFB-Cursos/Validation/validation.js
+++ CFB-Cursos/Validation/validation.js
@@ -2,7 +2,7 @@ const f_nome=document.querySelector("#f_nome")
 const f_nota=document.querySelector("#f_nota")
 const f_msg=document.querySelector("#f_msg")
 
-document.querySelector("btn_validar").addEventListener("click",(event)=>{
+document.querySelector("#btn_validar").addEventListener("click",(event)=>{
     let msg=null
     if(!f_nota.checkValidity()){            //Checa se o formulário foi preenchido corretamente ou não
         msg=f_nota.validationMessage        //Seleciona a msg de acordo com o erro de preenchimento 
