fffbfab2edefbc76cde0660c15b2653ad0c04a6f
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-02-21 22:32:13 -0300

    update fetch for APIs

--- CFB-Cursos/Fetch_APIs/fetch01.js
+++ CFB-Cursos/Fetch_APIs/fetch01.js
@@ -1,9 +1,20 @@
-const endpoint = "https://cfbcursos.repl.co/"
-fetch(endpoint)             //faz o consumo da API
-.then(res => res.json())    //'then' Recebe o retorno da API e armazena na variavel 'res'
-                            //'json()' Método que converte os dados para Json
-.then(dados=>{              //Recebe o retorno da API já tratada como Json e armazena na variavel 'dados'
-    console.log(dados)
-})                
+const p_temp = document.getElementById("p_temp")
+const p_nivel = document.getElementById("p_nivel")
+const p_press = document.getElementById("p_press")
+
+const obterDados=()=>{
+    const endpoint = "https://cfbcursos.repl.co/"
+    fetch(endpoint)             //faz o consumo da API
+    .then(res => res.json())    //'then' Recebe o retorno da API e armazena na variavel 'res'
+                                //'json()' Método que converte os dados para Json
+    .then(dados=>{              //Recebe o retorno da API já tratada como Json e armazena na variavel 'dados'
+        console.log(dados)
+        p_temp.innerHTML="Temperatura: "+ dados.temperatura
+        p_nivel.innerHTML="Nivel: "+ dados.nivel
+        p_press.innerHTML="Pressão: "+ dados.pressao
+    })             
+}
+
+let intervalo=setInterval(obterDados,3000)
 
 
