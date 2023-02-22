3c794191d9744910a9c2a904d615d2038d58401a
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-02-22 19:35:33 -0300

    Update fetch01.js

--- CFB-Cursos/Fetch_APIs/fetch01.js
+++ CFB-Cursos/Fetch_APIs/fetch01.js
@@ -1,9 +1,11 @@
 const p_temp = document.getElementById("p_temp")
 const p_nivel = document.getElementById("p_nivel")
 const p_press = document.getElementById("p_press")
+const btn_texto = document.getElementById("btn_texto")
 
+//Recebendo dados da API
 const obterDados=()=>{
-    const endpoint = "https://cfbcursos.repl.co/"
+    const endpoint = "https://cfbcursos.repl.co/"   //Endereço da API
     fetch(endpoint)             //faz o consumo da API
     .then(res => res.json())    //'then' Recebe o retorno da API e armazena na variavel 'res'
                                 //'json()' Método que converte os dados para Json
@@ -17,4 +19,27 @@ const obterDados=()=>{
 
 let intervalo=setInterval(obterDados,3000)
 
+//Enviando dados para a API
+let dados = {
+    nome: "Carlos",
+    canal: "CFBCursos",
+    curso: "Javascript"
+}
 
+let cabecalho = {
+    method: "POST",
+    body: JSON.stringify(dados)
+}
+
+const gravarDados=()=>{
+    const endpoint = "https://cfbcursos.repl.co/"
+    fetch(endpoint, cabecalho)
+    .then(res => res.json())
+    .then(ret=>{
+        console.log(ret)
+    })
+}
+
+btn_texto.addEventListener("click", (event)=>{
+    gravarDados()
+})
