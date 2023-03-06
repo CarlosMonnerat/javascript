5413e571a1a01061e8047f33e4f82344f0ab9443
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-03-06 18:47:26 -0300

    Update topo.js

--- CFB-Cursos/reuse/topo.js
+++ CFB-Cursos/reuse/topo.js
@@ -9,3 +9,17 @@ const topo = document.createElement("div")
 topo.setAttribute("id","topo")
 topo.setAttribute("style", estilotopo)
 body.prepend(topo)
+
+const estilo_img_logo = "width: 200px"
+const logo = 
+    "<div id='logo' class='logo'>"+
+        "<img src='logo.png' tittle='CFBCursos' style='"+estilo_img_logo+"'/>"+
+    "</div>"
+topo.innerHTML += logo
+
+const login =
+    "<div id='login' class='login'>"+
+        "<p id='matricula'>Matricula:<span></span></p>"+
+        "<p id='nome'>Nome:<span></span></p>"+
+    "</div>"
+topo,innerHTML += login
