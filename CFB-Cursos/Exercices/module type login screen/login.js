cab063ecf0b919f0c56b914aa2f01d7e830a8e43
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-03-30 19:52:23 -0300

    update ex login screen

--- CFB-Cursos/Exercices/module type login screen/login.js
+++ CFB-Cursos/Exercices/module type login screen/login.js
@@ -7,11 +7,19 @@ class Login{
     //https://loginv1.cfbcursos.repl.co/?matricula=123&senha=321
 
     static login=(mat,pass)=>{
-        this.endpoint+=`?matricula=${mat}&senha=${pass}`
+        this.endpoint+=`?matricula=${mat}&senha=${pass}`;
         fetch(this.endpoint)
         .then(res=>res.json())
         .then(res=>{
-            
+            if(res){
+                this.logado = true;
+                this.matlogado = mat;
+                this.nomelogado = res.nome;
+                this.acessologado = res.acesso;
+                console.log(res)
+            }else{
+                console.log("Usuário não encontrado!")
+            }
         })
     }
 }
