4288633754dd42529d33036fced2f9adb527ea54
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-05-12 18:23:58 -0300

    getting the data from products.json

--- CFB-Cursos/DataGridView/script.js
+++ CFB-Cursos/DataGridView/script.js
@@ -1,11 +1,53 @@
 const config_dgv={
     endpoint:"products.json",
+    idDestino:"dgvDados",
 }
 const dgv=(config_dgv)=>{
+    const dgvDados = document.getElementById(config_dgv.idDestino);
     fetch(config_dgv.endpoint)
     .then(res=>res.json())
     .then(res=>{
+        res.forEach(element => {
+            const dgvLinha = document.createElement("div");
+            dgvLinha.setAttribute("class","dgvLinha");
+            
+            const c1 = document.createElement("div");
+            c1.setAttribute("class","c1");
+            c1.innerHTML=element.id;
+            dgvLinha.appendChild(c1);
+
+            const c2 = document.createElement("div");
+            c2.setAttribute("class","c2");
+            c2.innerHTML=element.produto;
+            dgvLinha.appendChild(c2);
+
+            const c3 = document.createElement("div");
+            c3.setAttribute("class","c3");
+            c3.innerHTML=element.marca;
+            dgvLinha.appendChild(c3);
+
+            const c4 = document.createElement("div");
+            c4.setAttribute("class","c4");
+            c4.innerHTML=element.modelo;
+            dgvLinha.appendChild(c4);
+
+            const c5 = document.createElement("div");
+            c5.setAttribute("class","c5");
+            c5.innerHTML="D E V";
+            dgvLinha.appendChild(c5);
+
+            dgvDados.appendChild(dgvLinha);
+        });
         console.log(res)
     })
 }
 
+dgv(config_dgv);
+
+    // <div class="dgvLinha">
+    //     <div class="c1">01</div>
+    //     <div class="c2">processador</div>
+    //     <div class="c3">Intel</div>
+    //     <div class="c4">i7</div>
+    //     <div class="c5">D E V</div>
+    // </div>
