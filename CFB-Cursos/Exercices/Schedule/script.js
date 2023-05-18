483e8c1baa2c5a879d376f39814e5d2d39ce189c
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-05-18 20:45:27 -0300

    added tabs function

--- CFB-Cursos/Exercices/Schedule/script.js
+++ CFB-Cursos/Exercices/Schedule/script.js
@@ -6,3 +6,23 @@ const btn_pesquisar = document.querySelector("#btn_pesquisar");
 const btn_gestao = document.querySelector("#btn_gestao");
 const btn_sobre = document.querySelector("#btn_sobre");
 const principal = document.querySelector("#principal");
+
+btn_home.addEventListener("click",()=>{
+    window.open("./pages/home.html","if_principal");
+});
+
+btn_novo.addEventListener("click",()=>{
+    window.open("./pages/novo.html","if_principal");
+});
+
+btn_pesquisar.addEventListener("click",()=>{
+    window.open("./pages/pesquisar.html","if_principal");
+});
+
+btn_gestao.addEventListener("click",()=>{
+    window.open("./pages/gestao.html","if_principal");
+});
+
+btn_sobre.addEventListener("click",()=>{
+    window.open("./pages/sobre.html","if_principal");
+});
