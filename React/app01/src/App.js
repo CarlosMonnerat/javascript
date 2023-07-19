0c7bbe7a9bfd140d7eeb5e183dfd3f763343dc83
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-07-19 17:38:31 -0300

    update react

--- React/app01/src/App.js
+++ React/app01/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import Logo from './componentes/imgs/logo.png'
+import menina from './componentes/imgs/menina.jpeg'
 
 export default function App(){
+  const canal=()=>{
+    return('CFB Cursos')
+  }
+  function curso(){
+    return('Curso de React')
+  }
+
   return(
-    <>
-      <p>CFB Cursos</p>
-      <br/>
-      <p>Curso de react</p>
-    </>
+    <section>
+      <p>Canal: {canal()}</p>
+      <p>{'Curso: ' + curso()}</p>
+      <img src={Logo}></img>
+      <img src={menina}></img>
+    </section>
   )
 }
