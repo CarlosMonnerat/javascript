9cb688ada113fa7e4979fd24a357f41a6249a677
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-07-22 19:03:29 -0300

    update app.js

--- React/app01/src/App.js
+++ React/app01/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import Header from './componentes/Header'
 import Corpo from './componentes/Corpo'
+import Dados from './componentes/Dados'
 
 export default function App(){
+
+  const cnl = 'CFB Cursos'
+  const yt = 'youtube.com/cfbcursos'
+  const crs = 'React.js'
+
   return(
     <>
       <Header/>
       <Corpo/>
+      <Dados canal={cnl} youtube={yt} curso={crs}/>
     </>
   )
 }
