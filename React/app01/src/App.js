b25fc214ca6ff4c7c7b54a287df7255d3e1884d1
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-08-08 15:51:21 -0300

    Update State app01.js

--- React/app01/src/App.js
+++ React/app01/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Numero from './componentes/Numero'
 import './App.css'
 
 export default function App(){
+
+  const [num, setNum]=useState(10)
   return(
     <>
-  
+      <p>Valor do state num em App: {num}</p>
+      <Numero num={num} setNum={setNum}/>
     </>
   )
 }
