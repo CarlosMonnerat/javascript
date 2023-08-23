736d528abed9a2aa6aaaa592c6b1401f3ac6cc45
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-08-22 21:25:59 -0300

    update app01

--- React/app01/src/App.js
+++ React/app01/src/App.js
@@ -1,17 +1,11 @@
 import React from 'react'
-import logo from './componentes/imgs/logo.png'
+import HelloWord from './componentes/HelloWord' //Importa o componente
 
 export default function App(){
   
   return(
     <>
-      <div>
-        <h1>Olá Mundo!</h1>
-        <p>Meu primeiro App</p>
-        <img src='/img/menina.jpeg' alt="Imagem na msm pasta do Index.html"/>
-        <img src={logo} alt="imagem fora da pasta do Index.html"/> 
-         
-      </div>
+      <HelloWord/>
     </>
   )
 }
---
+++ React/app01/src/componentes/HelloWord.js
@@ -0,0 +1,12 @@
+import Frase from './Frase'
+
+function HelloWord(){ //A Função base tem o mesmo nome do componente
+    return(
+        <>
+            <h1>Meu primeiro componente</h1>
+            <Frase/>   
+        </>
+    )
+}
+
+export default HelloWord
