18cc890f040129ead7d9bd4681949b4091ccfb4c
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-09-01 17:32:42 -0300

    Update css app01

--- React/app01/src/App.js
+++ React/app01/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Pessoa from './componentes/Pessoa'
+import Frase from './componentes/Frase'
 
 export default function App(){
   
   return(
     <div>
       <h1>Testando o CSS</h1>
+      <Frase/>
       <Pessoa 
         nome="Carlos"
         idade="28" 
