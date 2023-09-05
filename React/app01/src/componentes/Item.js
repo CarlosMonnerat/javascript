c5eed5342be48b93e37b39de2e8df095ed3ec4a3
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-09-05 19:04:24 -0300

    update app01

--- React/app01/src/componentes/Item.js
+++ React/app01/src/componentes/Item.js
@@ -1,10 +1,17 @@
-function Item(props){
+import PropTypes from 'prop-types' // Importa o pacote do React
+
+function Item({marca, ano_lancamento}){
     return(
         <>
-            <li>{props.marca}</li>
-            <p>Teste</p>
+            <li>{marca} - {ano_lancamento}</li>
+            
         </>
     )
+}
+
+Item.propTypes = { // Vai garantir que as propriedades passadas sejam do tipo 'string' e 'number' respectivamente.
+    marca: PropTypes.string,
+    ano_lancamento: PropTypes.number,
 }
 
 export default Item
