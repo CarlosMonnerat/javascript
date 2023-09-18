11c928d9534df277f39c00770826c5501c8f0638
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-09-18 14:02:59 -0300

    create Evento.js

--- React/app01/src/App.js
+++ React/app01/src/App.js
@@ -1,21 +1,14 @@
 import React from 'react'
-import Pessoa from './componentes/Pessoa'
-import Frase from './componentes/Frase'
-import List from './componentes/List'
+import Evento from './componentes/Evento'
+
 
 export default function App(){
   
   return(
     <div>
-      <h1>Testando o CSS</h1>
-      <Frase/>
-      <Pessoa 
-        nome="Carlos"
-        idade="28" 
-        profissao="Programador" 
-        foto="https://via.placeholder.com/150"
-      />
-      <List/>
+      <h1>Testando Eventos</h1>
+      <Evento/>
+      
     </div>
   )
 }
