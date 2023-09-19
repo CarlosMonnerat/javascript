82fcc4b1f740facfe98e63ed16152c3019ee721f
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-09-19 16:09:23 -0300

    update app01

--- React/app01/src/componentes/Form.js
+++ React/app01/src/componentes/Form.js
@@ -1,14 +1,29 @@
+import { useState } from 'react'        //Importa o Hook do useState
+
 function Form(){
     function cadastrarusuario(e){
-        e.preventDefault()      //Interrompe o envio do formulário para servidor
-        console.log("Cadastrou o usuário!")
+        e.preventDefault()              //Interrompe o envio do formulário para servidor
+        console.log(`Usuário ${name} foi cadastrado com a senha: ${password}`)
     }
+
+    const [name, setName] = useState()  //[Variável recebida, variável que será alterada]
+    const [password, setPassword] = useState()
+
     return(
         <div>
             <h1>Meu cadastro:</h1>
             <form onSubmit={cadastrarusuario}>
                 <div>
-                    <input type="text" placeholder="Digite seu nome"></input>
+                    <label htmlFor="name">Nome:</label>
+                    <input type="text" id="name" name="name" placeholder="Digite seu nome"
+                    onChange={(e) => setName(e.target.value)}>
+                    </input>
+                </div>
+                <div>
+                    <label htmlFor="password">Senha:</label>
+                    <input type="password" id="password" name="password" placeholder="Digite sua senha"
+                    onChange={(e) => setPassword(e.target.value)}>
+                    </input>
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar"></input>
