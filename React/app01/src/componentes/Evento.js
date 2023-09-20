0dc10c1b21dbc33d9ad79aff18d6c8fac21a41e6
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-09-20 15:45:46 -0300

    Update Evento.js

--- React/app01/src/componentes/Evento.js
+++ React/app01/src/componentes/Evento.js
@@ -1,14 +1,19 @@
 import Button from './evento/Button'
 
 function Evento(){
-    function meuEvento(){
+    function meuEvento(){           //Método 1
         console.log(`Ativando o primeiro evento!`)
+    }
+
+    function segundoEvento(){       //Método 2
+        console.log(`Ativando o segundo evento!`)
     }
 
     return(
         <div>
             <p>Clique para disparar um evento</p>
             <Button event={meuEvento} text="Primeiro Evento"/>
+            <Button event={segundoEvento} text="Segundo Evento"/>
         </div>
     )
 }
