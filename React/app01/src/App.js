3cbd13f129c28ee1a476cfb830100d94f5c27245
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-09-21 15:53:24 -0300

    update app01

--- React/app01/src/App.js
+++ React/app01/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import Condicional from './componentes/Condicional'
+import OutraLista from './componentes/OutraLista'
 
 export default function App(){
   
+  const meusItens = ['React', 'Vue', 'Angular']
+
   return(
     <div>
-      <h1>Renderização Condicional</h1>
-      <Condicional/>    
+      <h1>Renderização de Listas</h1>
+      <OutraLista itens={meusItens}/>
     </div>
   )
 }
