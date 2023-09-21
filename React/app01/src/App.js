6def1151ca4a1ab7a3af4814085329d54c5233c2
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-09-21 16:38:18 -0300

    create state lift

--- React/app01/src/App.js
+++ React/app01/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import OutraLista from './componentes/OutraLista'
+import { useState } from 'react'
+import SeuNome from './componentes/SeuNome'
 
 export default function App(){
-  
-  const meusItens = ['React', 'Vue', 'Angular']
+  const [nome, setNome] = useState()
 
   return(
     <div>
-      <h1>Renderização de Listas</h1>
-      <OutraLista itens={meusItens}/>
-      <OutraLista itens={[]}/>
+      <h1>State Lift</h1>
+      <SeuNome setNome = {setNome}/>
+      {nome}
     </div>
   )
 }
