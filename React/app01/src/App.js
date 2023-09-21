4f9461714b248c69511654a3bf97d070e816660f
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-09-21 16:06:18 -0300

    update app01

--- React/app01/src/App.js
+++ React/app01/src/App.js
@@ -9,6 +9,7 @@ export default function App(){
     <div>
       <h1>Renderização de Listas</h1>
       <OutraLista itens={meusItens}/>
+      <OutraLista itens={[]}/>
     </div>
   )
 }
--- React/app01/src/componentes/OutraLista.js
+++ React/app01/src/componentes/OutraLista.js
@@ -2,10 +2,11 @@ function OutraLista({ itens }){
     return(
         <>
             <h3>Lista de Frameworks:</h3>
-            {
-                itens.map((item, index) => (
-                    <p key={index}>{item}</p>
-                ))
+            {itens.length > 0 ? ( 
+                itens.map((item, index) => <p key={index}>{item}</p>)
+            ) : ( 
+                    <p>Não há itens na lista</p> 
+                )    
             }
         </>
     )
