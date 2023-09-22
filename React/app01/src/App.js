e82c90377104995ed9d47c3ae9bba0b8fa7fe559
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-09-22 17:56:47 -0300

    update app01

--- React/app01/src/App.js
+++ React/app01/src/App.js
@@ -1,26 +1,11 @@
 import React from "react"
-import{
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate
-} from 'react-router-dom'
-import Home from './pages/Home'
-import Contato from './pages/Contato'
-import Users from './pages/Users'
+
 
 function App(){
   return(
-    <Router>
-      <Routes>
-
-        <Route path="/" exact element={<Home />} />  
-        <Route path="/users" element={<Users />} />  
-        <Route path="/allusers" element={<Navigate to="/users"/>} />
-        <Route path="/Contato" element={<Contato />} />
-          
-      </Routes>
-    </Router>
+    <div>
+      <h1>Olá mundo!</h1>
+    </div>
   )
 }
 
