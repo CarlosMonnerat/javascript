973cf89ca5974ba780abd615e9202a911f80c9ff
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-09-22 19:30:28 -0300

    update app01 -  Error: 'useRef'

--- React/app01/src/App.js
+++ React/app01/src/App.js
@@ -1,8 +1,9 @@
 import React from "react"
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import Empresa from './pages/Empresa'
 import Contato from './pages/Contato'
+import Users from './pages/Users'
 import Navbar from './componentes/layout/Navbar'
 import Footer from './componentes/layout/Footer'
 
@@ -12,7 +13,9 @@ function App(){
       <Navbar />
 
       <Routes>
-        <Route exact path="/" element={<Home />}/>
+        <Route path="/" element={<Home />}/>
+        <Route path="/users" element={<Users />}/>
+        <Route path="/allusers" element={<Navigate to="/users" />} />
         <Route path="/empresa" element={<Empresa />}/>
         <Route path="/contato" element={<Contato />}/>
       </Routes>
