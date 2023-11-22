1fb805ca1ff1cb168bccf7cda4aa0505f1ecc7c7
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-11-22 14:50:11 -0300

    update app imc

--- React/imc/src/App.js
+++ React/imc/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+
+const tabelaIMC=()=>{
+  return(
+    <table border='1' style={{borderCollapse:'collapse'}}>
+      <thead>
+        <tr>
+          <th>Classificação</th>
+          <th>IMC</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Abaixo do Peso</td>
+          <td>Abaixo de 18,5</td>
+        </tr>
+        <tr>
+          <td>Peso Normal</td>
+          <td>Entre 18,5 e 24,9</td>
+        </tr>
+        <tr>
+          <td>Sobrepeso</td>
+          <td>Entre 25 e 29,9</td>
+        </tr>
+        <tr>
+          <td>Obesidade Grau I</td>
+          <td>Entre 30 e 34,9</td>
+        </tr>
+        <tr>
+          <td>Obesidade Grau II</td>
+          <td>Entre 35 e 39,9</td>
+        </tr>
+        <tr>
+          <td>Obesidade Grau III</td>
+          <td>Acima de 40</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <h1>Calculadora IMC</h1>
     </div>
-  );
+  )
 }
 
 export default App;
