bbeba2dbf629494952aca69b90f7555eefef54b4
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2024-01-02 20:57:54 -0300

    Update TabelaIMC.js

--- React/imc/src/components/TabelaIMC.js
+++ React/imc/src/components/TabelaIMC.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-export default function TabelaIMC(){
+export default class TabelaIMC extends React.Component{
+  render(){
     return(
         <table border='1' style={{borderCollapse:'collapse'}}>
           <thead>
@@ -36,5 +37,6 @@ export default function TabelaIMC(){
             </tr>
           </tbody>
         </table>
-      )
+    )
+  }    
 }
