2077cdd08c2a6fcf4c9b7d082ce9feb38df62ee5
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2024-01-15 15:44:41 -0300

    Update App.js

--- React/calculadora/src/App.js
+++ React/calculadora/src/App.js
@@ -15,6 +15,13 @@ export default function App() {
     )
   }
 
+  const Btn=(label,onClick)=>{
+    return(
+      <button style={cssBtn} onClick={onClick}>{label}</button>
+    )
+
+  }
+
   //ESTILOS
   const csstela={
     display: 'flex',
@@ -38,6 +45,17 @@ export default function App() {
     color:'#fff',
   }
   
+  const cssBtn={
+    fontSize:30,
+    height:75,
+    width:75,
+    padding:20,
+    backgroundColor:'#000',
+    color:'#fff',
+    borderColor:'#000',
+    textAlign:'center',
+    outline:'none'
+  }
 
   return (
     <div>
