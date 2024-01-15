ec696a165c53a85de288c45f10439847ea602bfd
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2024-01-15 15:25:50 -0300

    Update App.js

--- React/calculadora/src/App.js
+++ React/calculadora/src/App.js
@@ -1,6 +1,11 @@
 import React, {useState} from 'react'
 
 export default function App() {
+  const [valorTela, setValorTela]=useState('')
+  const [resultado, setResultado]=useState(0)
+  const [acumulador, setAcumulador]=useState(0)
+  const [operado, setoperado]=useState(false)
+
   return (
     <div>
       <h1>Olá mundo</h1>
