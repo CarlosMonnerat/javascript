b5a09ee45acf3ec2853708dad57ca5198345304f
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2024-01-16 16:00:37 -0300

    Update App.js

--- React/calculadora/src/App.js
+++ React/calculadora/src/App.js
@@ -118,8 +118,33 @@ export default function App() {
 
   //Retorno do App
   return (
-    <div>
-      <h1>Olá mundo</h1>
+    <div style={cssContainer}>
+      <h3>Calculadora Matemática Simples</h3>
+      {Tela(valorTela, resultado)}
+      <div style={cssBotoes}>
+        {Btn('AC', limparMemoria)}
+        {Btn('(', ()=>addDigitoTela('('))}
+        {Btn(')', ()=>addDigitoTela(')'))}
+        {Btn('/', ()=>addDigitoTela('/'))}
+        {Btn('7', ()=>addDigitoTela('7'))}
+        {Btn('8', ()=>addDigitoTela('8'))}
+        {Btn('9', ()=>addDigitoTela('9'))}
+        {Btn('*', ()=>addDigitoTela('*'))}
+        {Btn('4', ()=>addDigitoTela('4'))}
+        {Btn('5', ()=>addDigitoTela('5'))}
+        {Btn('6', ()=>addDigitoTela('6'))}
+        {Btn('-', ()=>addDigitoTela('-'))}
+        {Btn('1', ()=>addDigitoTela('1'))}
+        {Btn('2', ()=>addDigitoTela('2'))}
+        {Btn('3', ()=>addDigitoTela('3'))}
+        {Btn('+', ()=>addDigitoTela('+'))}
+        {Btn('0', ()=>addDigitoTela('0'))}
+        {Btn('.', ()=>addDigitoTela('.'))}
+        {Btn('<', ()=>operacao('bs'))}
+        {Btn('=', ()=>operacao('='))}
+
+      </div>
+
     </div>
       
   );
