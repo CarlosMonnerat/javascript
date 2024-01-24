52c4fe4007e40b801194612ddc53df6ea948d0b7
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2024-01-24 16:38:23 -0300

    Update App.js

--- React/jogo/src/App.js
+++ React/jogo/src/App.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function App() {
+export default function App() {
+  //ESTILOS
+  const tabu={
+    display: 'flex',
+    flexDirection: 'column'
+  }
+  
   return (
     <div>
       <h1>Olá mundo</h1>
@@ -9,4 +15,3 @@ function App() {
   );
 }
 
-export default App;
