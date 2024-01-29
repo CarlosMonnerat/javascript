a107abd9fbe749b7dfc2a68c8098c36dc281d6cd
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2024-01-29 16:35:31 -0300

    Update App.js

--- React/jogo/src/App.js
+++ React/jogo/src/App.js
@@ -27,7 +27,22 @@ export default function App() {
   const [simboloAtual, setSimboloAtual]=useState('X')
   const [jogando, setJogando]=useState(true)
 
-  
+  const tabuleiro=(j)=>{
+    return(
+      <div style={tabu}>
+        <div style={tabuLinha}>
+          <div style={casa} data-pos='00' onClick="">{j[0][0]}</div>
+          <div style={casa} data-pos='01' onClick="">{j[0][1]}</div>
+          <div style={casa} data-pos='02' onClick="">{j[0][2]}</div>  
+        </div>
+        <div style={tabuLinha}>
+          <div style={casa} data-pos='10' onClick="">{j[1][0]}</div>
+          <div style={casa} data-pos='11' onClick="">{j[1][1]}</div>
+          <div style={casa} data-pos='12' onClick="">{j[1][2]}</div>  
+        </div>
+      </div>
+    )
+  }
 
 
   return (
