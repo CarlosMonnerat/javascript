bb4cdfd14af9be116ab1055e01294b5fc6e50751
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2024-01-29 16:06:25 -0300

    Update App.js

--- React/jogo/src/App.js
+++ React/jogo/src/App.js
@@ -10,7 +10,18 @@ export default function App() {
     display: 'flex',
     flexDirection: 'row'
   }
-  
+  const casa={
+    width:100,
+    heigth:100,
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    flexDirection:'row',
+    cursor:'pointer',
+    fontSize:60,
+    border:'1px solid #000'
+  }
+
   return (
     <div>
       <h1>Olá mundo</h1>
