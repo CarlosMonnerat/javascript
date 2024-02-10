7e7f1e17224dd4e6c979b3b72ba4f25f9aca3116
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2024-02-09 23:07:58 -0300

    create modulo F

---
+++ CursoemVideo/modulo F - advancing studies/Aula#15-vetores/arrays.js
@@ -0,0 +1,20 @@
+let num = [5,8,2,9,3]
+let vec = [8,4,1,3,9,6,7,5,2]
+
+
+num[2]=7                                                                            //Substitui um elemento indicado no vetor! (OBs: O vetor começa em ZERO!)
+num.push(17)                                                                        //acrescenta um elemento diretamente ao final do vetor
+
+vec.length                                                                          // Diz quantos elementos tem o vetor 
+vec.sort()                                                                          // Organiza o vetor em ordem crescente
+
+console.log (`Nosso vetor "num" é o ${num}`)
+console.log (`O vetor "vec" em ordem crescente é: [${vec}]`)
+console.log (`O vetor "vec" tem ${vec.length} elementos`)
+console.log (vec[0])                                                                   //Mostra qual elemento está na posição indicada
+
+console.log ('imprimindo o vetor com o comando FOR:')
+
+for(let pos=0; pos < num.length; pos++) {
+    console.log (num[pos])
+}
---
+++ CursoemVideo/modulo F - advancing studies/Aula#16 -Funções/funçao01(parimpar).js
@@ -0,0 +1,10 @@
+function parimpar (n){
+    if (n % 2 == 0){
+        return `Par!`
+    }else {
+        return `Impar!`
+    }
+}
+
+let res = parimpar(226)
+ console.log(res)                                       // Ou "console.log(parimpar()" Tbm funciona
