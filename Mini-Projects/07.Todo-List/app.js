b7e652f8bc0041edea9a584fecec7c25c102fe8d
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2024-10-26 17:01:22 -0300

    Update app.js

--- Mini-Projects/07.Todo-List/app.js
+++ Mini-Projects/07.Todo-List/app.js
@@ -23,21 +23,22 @@ const criarItem = (tarefa, status, indice) => {                                 
     document.getElementById('todoList').appendChild(item);
 }
 
-const clear = () => {                                                                           //Limpa as tarefas da tela
+const clear = () => {                                             //Limpa as tarefas da tela
     const todoList = document.getElementById('todoList');   
-    while(todoList.firstChild){                                                                 //Enquanto existir o primeiro filho, remove o último filho
+    while(todoList.firstChild){                                  //Enquanto existir o primeiro filho, remove o último filho
         todoList.removeChild(todoList.lastChild);
     }
 
 }
 
-const render = () => {                                                                          //Lê o banco de dados e Atualiza a tela
-    clear();                                                                                    //Limpa a tela cada vez que o Render é chamado para não haver duplicações
+const render = () => {                                           //Lê o banco de dados e Atualiza a tela
+    clear();                                                     //Limpa a tela cada vez que o Render é chamado para não haver duplicações
     const banco = getBanco();
-    banco.forEach((item, indice) => criarItem(item.tarefa, item.status, indice));               //Para cada item do banco, pega um item e o indice do array, e joga para a função 'criarItem()' informando seus parametro
+    banco.forEach((item, indice) => criarItem(item.tarefa, item.status, indice));
+//Para cada item do banco, pega um item e o indice do array, e joga para a função 'criarItem()' informando seus parametro
 }
 
-const addTarefa = (evento) => {                                                                 //Recebe a tarefa digitada pelo usuário e add no Array (banco de dados)
+const addTarefa = (evento) => {                                 //Recebe a tarefa digitada pelo usuário e add no Array (banco de dados)
     const tecla = evento.key;
     const texto = evento.target.value
     if(tecla == 'Enter' && texto != ''){
@@ -51,24 +52,24 @@ const addTarefa = (evento) => {                                                 
 document.getElementById('newItem').addEventListener('keypress', addTarefa);
 
 
-const removerItem = (indice) => {                                                               //Remove os Itens do banco de dados
+const removerItem = (indice) => {                               //Remove os Itens do banco de dados
     const banco = getBanco();
-    banco.splice(indice, 1);                                                                    //splice recorta um array. (a partir, remove nº) no caso, ele próprio
+    banco.splice(indice, 1);                                    //splice recorta um array. (a partir, remove nº) no caso, ele próprio
     setBanco(banco);
     render();
 }
 
-const marcarItem = (indice) => {                                                                //Marca ou desmarca o checkbox das tarefas no banco de dados
+const marcarItem = (indice) => {                               //Marca ou desmarca o checkbox das tarefas no banco de dados
     const banco = getBanco();
     banco[indice].status = banco[indice].status == '' ? 'checked' : '';
     setBanco(banco);
     render();
 }
 
-const clickItem = (evento) => {                                                                 //Verifica onde o usuário clicou e encaminha para a função determinada
+const clickItem = (evento) => {                               //Verifica onde o usuário clicou e encaminha para a função determinada
     const elemento = evento.target;
     if(elemento.type == 'button'){
-        const indice = elemento.dataset.indice;                                                 //dataset captura o npumero do indice de mesmo nome usado na função criarItem() 
+        const indice = elemento.dataset.indice;              //dataset captura o npumero do indice de mesmo nome usado na função criarItem() 
         removerItem(indice);
     }else if(elemento.type == 'checkbox'){
         const indice = elemento.dataset.indice;
