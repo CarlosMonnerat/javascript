8914403491d817045946ba4cd33c35d8dc0c1090
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2024-12-16 18:36:56 -0300

    Update main.js

--- Mini-Projects/08.Crud/main.js
+++ Mini-Projects/08.Crud/main.js
@@ -4,17 +4,23 @@ const openModal = () => document.getElementById('modal').classList.add('active')
 const closeModal = () => document.getElementById('modal').classList.remove('active');
 
 const tempClient = {
-    nome: "Bruna",
-    email: "brunar@gmail.com",
+    nome: "Helena",
+    email: "helenam@gmail.com",
     celular: "21987643210",
     cidade: "Teresópolis"
 }
 
-//CRUD - create, read, update, delete
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_Client')) ?? [];
+const setLocalStorage = (dbClient) => localStorage.setItem("db_Client", JSON.stringify(dbClient));
+
+// CRUD - create, read, update, delete
+
+// CRUD - CREATE
 const createClient = (client) =>{
-    const db_client = JSON.parse(localStorage.getItem('db_client'));
-    db_client.push(client);
-    localStorage.setItem("db_client", JSON.stringify(db_client));
+    const dbClient = getLocalStorage();
+    dbClient.push(client);
+    setLocalStorage(dbClient);
+    
 }
 
 //Eventos
