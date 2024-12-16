3f54201baa7178e5d41366e21c1f95b4403a044d
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2024-12-16 18:22:37 -0300

    Update main.js

--- Mini-Projects/08.Crud/main.js
+++ Mini-Projects/08.Crud/main.js
@@ -3,5 +3,20 @@
 const openModal = () => document.getElementById('modal').classList.add('active');
 const closeModal = () => document.getElementById('modal').classList.remove('active');
 
+const tempClient = {
+    nome: "Bruna",
+    email: "brunar@gmail.com",
+    celular: "21987643210",
+    cidade: "Teresópolis"
+}
+
+//CRUD - create, read, update, delete
+const createClient = (client) =>{
+    const db_client = JSON.parse(localStorage.getItem('db_client'));
+    db_client.push(client);
+    localStorage.setItem("db_client", JSON.stringify(db_client));
+}
+
+//Eventos
 document.getElementById('cadastrarCliente').addEventListener('click', openModal);
 document.getElementById('modalClose').addEventListener('click', closeModal);
