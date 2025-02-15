34db149fe97e431a973b2e94d1c5554808f5801e
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2025-02-15 19:35:10 -0300

    Update main.js

--- APIs/1-Via-CEP/js/main.js
+++ APIs/1-Via-CEP/js/main.js
@@ -1,10 +1,15 @@
 'use strict';
 
+const preencheFormulario = (dados) =>{
+    document.getElementById('endereco').value = dados.logradouro;
+}
+
 const pesquisarCep = async () =>{
     const cep = document.getElementById('cep').value;
     const url = `http://viacep.com.br/ws/${cep}/json/`;
     const response = await fetch(url);
     const dados = await response.json();
+    preencheFormulario(dados);
     console.log(dados);
 };
 
