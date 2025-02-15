1624925b1e2048cc643a2143a31132975d4f2274
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2025-02-15 19:38:04 -0300

    Update main.js

--- APIs/1-Via-CEP/js/main.js
+++ APIs/1-Via-CEP/js/main.js
@@ -2,7 +2,10 @@
 
 const preencheFormulario = (dados) =>{
     document.getElementById('endereco').value = dados.logradouro;
-}
+    document.getElementById('bairro').value = dados.bairro;
+    document.getElementById('cidade').value = dados.localidade;
+    document.getElementById('estado').value = dados.estado;    
+};
 
 const pesquisarCep = async () =>{
     const cep = document.getElementById('cep').value;
