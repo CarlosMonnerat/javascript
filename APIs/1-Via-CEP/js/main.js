798c1fd4f853c00d93d9d960bc9537cb120c9e7c
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2025-02-17 19:04:05 -0300

    Update main.js

--- APIs/1-Via-CEP/js/main.js
+++ APIs/1-Via-CEP/js/main.js
@@ -7,9 +7,18 @@ const preencheFormulario = (dados) =>{
     document.getElementById('estado').value = dados.estado;    
 };
 
+const limpaFormulario = () =>{
+    document.getElementById('endereco').value = "";
+    document.getElementById('bairro').value = "";
+    document.getElementById('cidade').value = "";
+    document.getElementById('estado').value = "";    
+};
+
+
 const cepValido = (cep) => cep.length == 8 && /^[0-9]+$/.test(cep);             //Testa se "cep" todos os caracteres estão na lista de 0 a 9
 
 const pesquisarCep = async () =>{
+    limpaFormulario();
     const cep = document.getElementById('cep').value;
     const url = `http://viacep.com.br/ws/${cep}/json/`;
     if(cepValido(cep)){
@@ -17,7 +26,6 @@ const pesquisarCep = async () =>{
         const dados = await response.json();
         if(dados.hasOwnProperty('erro')){                                       //Se este objeto possui a propriedade "erro"
             alert('CEP não encontrado. Verifique e tente novamente!');
-            console.log(dados);
         }else{
             preencheFormulario(dados);
         }
