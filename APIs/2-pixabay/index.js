9fc565399c838d240f232f67d9bbdc19aa14f023
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2025-02-21 18:06:14 -0300

    event.key and target.value

--- APIs/2-pixabay/index.js
+++ APIs/2-pixabay/index.js
@@ -1,2 +1,13 @@
 'use strict'
 
+const loadGallery = (textSearch) => {
+    console.log(textSearch);
+}
+
+const handleKeypress = ({key, target}) => {     //caracteristica do js chamada 'destructuring'. => (event.key, event.target)
+    if(key === 'Enter'){
+         loadGallery(target.value);
+    }   
+}
+
+document.getElementById('search-input').addEventListener('keypress', handleKeypress);
