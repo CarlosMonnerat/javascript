0bce935fd82f7a8249b1424ca49087aed7915b18
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2025-03-04 20:21:49 -0300

    Update app.js

--- APIs/3-Dog-API/app.js
+++ APIs/3-Dog-API/app.js
@@ -3,10 +3,16 @@
 const dogImage = document.getElementById('dog-image');
 
  async function updateDogImage(){
+    const breedInfo = document.getElementById('breed-name');
     const url = 'https://dog.ceo/api/breeds/image/random';
     const response = await fetch(url);
     const data = await response.json();
+    const breedName = data.message.split('/')[4];
+
     dogImage.src = data.message;
+    breedInfo.textContent = breedName;
+
+    
 };
 
 dogImage.addEventListener('click', updateDogImage);
