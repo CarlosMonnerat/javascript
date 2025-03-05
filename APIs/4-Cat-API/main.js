ad9839ca515e5329ee91cfe97e3774730b5a528b
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2025-03-05 12:38:07 -0300

    create project

---
+++ APIs/4-Cat-API/main.js
@@ -0,0 +1,10 @@
+'use strict'
+
+async function loadGallery (){
+    const gallery = document.getElementById("gallery");
+    const url = 'https://api.thecatapi.com/v1/images/search?limit=10';
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data);
+}
