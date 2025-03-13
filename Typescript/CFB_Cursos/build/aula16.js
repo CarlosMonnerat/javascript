91e0e3ee8d1ac2af87d994da504117148387b271
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2025-03-12 21:28:42 -0300

    Create aula16.js

---
+++ Typescript/CFB_Cursos/build/aula16.js
@@ -0,0 +1,17 @@
+"use strict";
+class Comput {
+    nome = "";
+    ram = 0;
+    cpu = 0;
+    ligado = false;
+}
+;
+const c1 = new Comput();
+const c2 = new Comput();
+const c3 = new Comput();
+c1.nome = "Rapidão";
+c2.nome = "Carão";
+c3.nome = "Gamer";
+console.log(c1.nome);
+console.log(c2.nome);
+console.log(c3.nome);
