dd90bbb0c74746335b988e18f4b971071093b134
carlos <carlosh.monnerat@gmail.com>
2025-04-01 18:33:34 -0300

    update project

--- APIs/5-user-API/app.js
+++ APIs/5-user-API/app.js
@@ -1,26 +1,25 @@
 'user strict'
 
-async function getUsers(){
-    const url = 'https://randomuser.me/api/?results=10&nat=BR';
+async function getUsers () {
+    const url = 'https://randomuser.me/api/?results=9&nat=BR';
     const response = await fetch(url);
     const data = await response.json();
     return data.results;
 };
 
-async function loadUsers(){
+async function loadUsers () {
     const users = await getUsers();
     const userContainer = document.getElementById('user-container');
     users.forEach(user => {
         const card = document.createElement('article');
-        card.className = 'userCard';
+        card.className = 'user-card';
         card.innerHTML = `
-            <img src= "${user.picture.large}" alt="Foto de ${user.name.first}" className="User-image">
+            <img src= "${user.picture.large}" alt="Foto de ${user.name.first}" class="user-image">
             <h2 Class="user-name"> ${user.name.first} ${user.name.last}</h2>
             <p>${user.email}</p>
             <p>${user.cell}</p>
         `
         userContainer.appendChild(card);
-        console.log(user);
     });
 };
 
