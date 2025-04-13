1598af94b06278a8839778f8f68c39b9ad59ec99
carlos <carlosh.monnerat@gmail.com>
2025-04-13 16:33:41 -0300

    Create Globais.ts

---
+++ Typescript/CFB_Cursos/src/Globais.ts
@@ -0,0 +1,8 @@
+export abstract class Globais {
+    public static teste: number = 0;
+    public static arrayTeste: number[] = [5,4,3,2,1];
+    public static objeto = {
+        nome: "Carlos",
+        canal: "CFB Cursos"
+    }
+};
