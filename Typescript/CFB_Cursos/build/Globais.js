e0ff48662a3f10f37f0d12fb7a581f91e4d5feab
carlos <carlosh.monnerat@gmail.com>
2025-04-20 11:43:36 -0300

    update

---
+++ Typescript/CFB_Cursos/build/Globais.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Globais = void 0;
+class Globais {
+    static teste = 0;
+    static arrayTeste = [5, 4, 3, 2, 1];
+    static objeto = {
+        nome: "Carlos",
+        canal: "CFB Cursos"
+    };
+}
+exports.Globais = Globais;
+;
