4930f061c2e2bbacfc07b0636606c730665751f3
carlos <carlosh.monnerat@gmail.com>
2025-05-01 19:43:34 -0300

    update

---
+++ NodeJS/CFB_Cursos/aulas/aula01-02.js
@@ -0,0 +1,9 @@
+//Criando um servidor Simples
+const http = require('http');
+
+http.createServer((request, response) => {
+   response.writeHead(200, {'Content-Type' : 'text/plain'});
+   response.write('CFB Cursos\nCurso de Node.js');
+   response.end();
+
+}).listen(1337);
