a540273eccb03b4cdfc156f5927af2a7e467903a
carlos <carlosh.monnerat@gmail.com>
2025-05-07 19:51:03 -0300

    Create aula05b.js

---
+++ NodeJS/CFB_Cursos/aulas/aula05b.js
@@ -0,0 +1,14 @@
+//Manipulando um arquivo com appendFile
+const http = require('http');
+const fs = require('fs');
+const porta = process.env.PORT;
+
+const server = http.createServer((req, res) =>{
+   fs.appendFile('teste.txt', 'Curso de Node - CFB Cursos', (err) =>{
+      if(err) throw err;
+      console.log('Arquivo Criado');
+      
+   });
+});
+
+server.listen(porta || 3000, () =>{console.log('Servidor Rodando!')});
