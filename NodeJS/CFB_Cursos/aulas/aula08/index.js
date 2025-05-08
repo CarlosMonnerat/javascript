b4ec6ccb3b073f2d148e08471c73fd5115fb70b2
carlos <carlosh.monnerat@gmail.com>
2025-05-08 19:30:58 -0300

    update

--- NodeJS/CFB_Cursos/aulas/aula08/index.js
+++ NodeJS/CFB_Cursos/aulas/aula08/index.js
@@ -1,5 +1,10 @@
 (async () => {
    const db = require('./db');
+   console.log('Inserir Novo cliente:');
+   const nom = "Brastorgilson";
+   const ida= "20";
+   await db.insereCliente({nome: nom, idade: ida});
+
    console.log('Obter todos os clientes');
    const clientes = await db.todosClientes();
    console.log(clientes);
