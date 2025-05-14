2016b853a81182c1aff24078248a047ab0b6132b
carlos <carlosh.monnerat@gmail.com>
2025-05-14 18:14:56 -0300

    Update index.js

--- NodeJS/CFB_Cursos/aulas/aula10/index.js
+++ NodeJS/CFB_Cursos/aulas/aula10/index.js
@@ -4,12 +4,25 @@ const formidable = require('formidable');
 const fs = require('fs');
 
 const servidor = http.createServer((req, res) => {
-   res.writeHead(200,{'Content-Type':'text/html'});
-   res.write('<form action="envioDeArquivo" method="post" enctype="multipart/form-data">');
-   res.write('<input type="file" name="filetoupload"><br>');
-   res.write('<input type="submit" value="Enviar">');
-   res.write('</form>');
-   return res.end();
+   if(req.url == '/envioDeArquivo'){
+      const form = new formidable.IncomingForm();
+      form.parse(req, (erro, campos, arquivos) => {
+         const urlantiga = arquivos.filetoupload.path;
+         const urlnova = 'C:/Repository/javascript/NodeJS/CFB_Cursos/aulas' + arquivos.filetoupload.name;
+         fs.rename(urlantiga, urlnova, (erro) => {
+            if(erro) throw erro;
+            res.write('Arquivo Movido!');
+            res.end();
+         });
+      });
+   }else{
+      res.writeHead(200, {'Content-Type':'text/html'});
+      res.write('<form action="envioDeArquivo" method="post" enctype="multipart/form-data">');
+      res.write('<input type="file" name="filetoupload"><br>');
+      res.write('<input type="submit" value="Enviar">');
+      res.write('</form>');
+      return res.end();
+   };
 });
 
 servidor.listen(porta);
