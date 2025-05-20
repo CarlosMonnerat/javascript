bbc4f8d46b074b1b63da0bbef960fbd2f76ed485
carlos <carlosh.monnerat@gmail.com>
2025-05-20 18:13:38 -0300

    Update app.js

--- APIs/7-Weather-API/app.js
+++ APIs/7-Weather-API/app.js
@@ -1,8 +1,18 @@
 'use strict';
 
+const searchWeather = async (town) => {
+   const key = 'f9488a76525c43ceafa210358252005';
+   const url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${town}&aqi=no`;
+   const response = await fetch(url);
+   const data = await response.json();
+   console.log(data);
+};
+
 const preencherForm = (event) => {
-   if(event.key === "Enter")  console.log(event.key);
-   
+   if(event.key === "Enter"){
+      const place = event.target.value;
+      const info = searchWeather(place);
+   }   
 };
 
 document.getElementById("weather-search").addEventListener('keydown', preencherForm);
