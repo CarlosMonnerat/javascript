668750173a254879764b8ff3e4a9d60d4190b0ec
carlos <carlosh.monnerat@gmail.com>
2025-05-20 18:49:57 -0300

    update

--- APIs/7-Weather-API/app.js
+++ APIs/7-Weather-API/app.js
@@ -9,10 +9,20 @@ const searchWeather = async (town) => {
 };
 
 const preencherForm = async (event) => {
+
    if(event.key === "Enter"){
       const place = event.target.value;
       const info = await searchWeather(place);
       console.log(info);
+      console.log(info.location.name);
+
+      document.getElementById('weather-image').src = info.current.condition.icon.replaceAll('64','128');
+      document.getElementById('weather-city').innerHTML = info.location.name;
+      document.getElementById('weather-temperature').innerHTML = info.current.temp_c;
+      document.getElementById('detail-speed').innerHTML = info.current.wind_degree;
+      document.getElementById('detail-humidity').innerHTML = info.current.humidity;
+
+
    }   
 };
 
