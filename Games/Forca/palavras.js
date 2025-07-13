115bb6feab76f6b103399f2b44dd4dce28d8159c
carlos <carlosh.monnerat@gmail.com>
2025-07-13 11:54:23 -0300

    create palavras.js

---
+++ Games/Forca/palavras.js
@@ -0,0 +1,11 @@
+export const biblioteca = [
+  "brinquedo","bicicleta", "carro", "televisao", "dinheiro", "shopping", "brincadeira","curso","computador","transporte","livraria",
+  "tecnologia","churrasco","impressora","telefone","internet","portaria","escola","carnaval","teclado","guitarra","bateria","chinelo",
+  "helicoptero","aviaçao","faculdade","martelo","amarelo","amigo","amor","branco","boneca","celular","elefante","furacao","girafa",
+  "papagaio","janela","limonada","passarinho","peixe","chaleira","onibus","caminhao","motocicleta","gelatina","parque","geladeira",
+  "cachorro","basquete","assombraçao","ardiloso","caminho","champanhe","chuveiro","coelho","contexto","coraçao","esqueleto","esquerda",
+  "impacto","modernidade","quarentena","refeiçao","reportagem","campeonato","amendoim","pedreiro","moeda","xicara","vermelho","serenata",
+  "biblioteca","caatinga","honra","cultura","exceçao","ambiçao","excesso","piedade","abstrato","paradoxo","comunhao","esperança",
+  "anatomia","nostalgia","compaixao","excelente","dicionario","disciplina","felicidade","significado","importante","conveniente",
+  "melancia","morango","morcego"
+];
