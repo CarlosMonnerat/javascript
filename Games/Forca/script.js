111541287fde37509a90aab9c49265c34ee1d784
carlos <carlosh.monnerat@gmail.com>
2025-07-13 11:55:56 -0300

    Update script.js

--- Games/Forca/script.js
+++ Games/Forca/script.js
@@ -21,17 +21,6 @@ document.addEventListener("DOMContentLoaded", function () {
     desenhos[6] = document.getElementById("pernaDimg");
     desenhos[7] = document.getElementById("cabeçaMimg");
 
-    // const biblioteca = [
-    //     "brinquedo", "bicicleta", "carro", "televisao", "dinheiro", "shopping", "brincadeira", "curso", "computador", "transporte", "livraria",
-    //     "tecnologia", "churrasco", "impressora", "telefone", "internet", "portaria", "escola", "carnaval", "teclado", "guitarra", "bateria",
-    //     "chinelo", "helicoptero", "aviaçao", "faculdade", "martelo", "amarelo", "amigo", "amor", "branco", "boneca", "celular", "elefante",
-    //     "furacao", "girafa", "papagaio", "janela", "limonada", "passarinho", "peixe", "chaleira", "onibus", "caminhao", "motocicleta",
-    //     "gelatina", "parque", "geladeira", "cachorro", "basquete", "assombraçao", "ardiloso", "caminho", "champanhe", "chuveiro", "coelho",
-    //     "contexto", "coraçao", "esqueleto", "esquerda", "impacto", "modernidade", "quarentena", "refeiçao", "reportagem", "campeonato",
-    //     "amendoim", "pedreiro", "moeda", "xicara", "vermelho", "serenata", "biblioteca", "caatinga", "honra", "cultura", "exceçao", "ambiçao",
-    //     "excesso", "piedade", "abstrato", "paradoxo", "comunhao", "esperança", "anatomia", "nostalgia", "compaixao", "excelente", "dicionario",
-    //     "disciplina", "felicidade", "significado", "importante", "conveniente", "melancia", "morango", "morcego"
-    // ];
 
     function criarInputs() {
         for (let i = 0; i < 20; i++) {
